16bafa46adee45eb6da1dc770addca0f7e979ebf
Ujjwal Upadhyay <ujjwalupadhyay2003@gmail.com>
2023-12-11 16:48:27 +0530

    Components -functional and class

---
+++ src/Components/Greet.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// function Greet() {
+//     return (
+//         <h1>hello world</h1>
+//     )
+// }
+
+export const Greet = () => <h1>This is using arrow function</h1>
+
+
+// export default Greet
