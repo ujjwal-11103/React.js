b2bed49a2c3cd643706f00a6646e1d0d9c810069
Ujjwal Upadhyay <ujjwalupadhyay2003@gmail.com>
2023-12-13 15:58:41 +0530

    Understanding why to use props and states

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import React from 'react';
-import States from './Components/States';
 
 function App(){
+
+  let data = 'Ujjwal'
+
+  function apple(){
+
+    data='prajwal'
+    console.log(data);
+    alert("Function called")
+    
+  }
+
   return(
-    <States/>
+<div>
+    <h1>Understanding why to use props and state</h1>
+    
+    <h2>Hello {data}</h2>
+
+{/* If we have used state method then it would get upfated */}
+    <h2>Hello again {data}</h2>                
+
+    {/* This is incorrect */}
+    {/* <button onClick={apple()}>Click</button>    */}
+
+    {/* Correct syntax */}
+    <button onClick={()=>apple()}>Click</button>   
+     
+    </div>
   )
 }
 export default App;
