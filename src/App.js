4a78769a682781e081796fa22772abde0f1d6fb8
Ujjwal Upadhyay <ujjwalupadhyay2003@gmail.com>
2023-12-13 11:00:27 +0530

    Implemented Props

--- src/App.js
+++ src/App.js
@@ -1,16 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
-// import Greet from './Components/Greet';
-import Welcome from './Components/Welcome'
-import { Greet } from './Components/Greet';
+// import logo from './logo.svg';
+// import './App.css';
+// import ChildComponent from './Components/Props.js'
+
+// fuction ParentComponent(){
+//   const
+// }
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Hello</h1>
+//       {/* simple passing argument from parent component to child component */}
+//       <ChildComponent name='Ujjwal' age={20} />
+
+//       {/* Passing objects to child components */}
+//       <ChildComponent person={{ surname: 'Upadhyay' }} age='21' />
+
+
+//       {/* const person={
+//         surname = "Upadhyay",
+//         age = 21,
+//         address = '123 society',
+//     }
+//     <ChildComponent person ={person}/> */}
+
+//     </div>
+//   );
+// }
+
+// export default App;
+// App.js
+import React from 'react';
+import ChildComponent from './Components/Props.js'
+
 function App() {
+  const person = {
+    name: 'John',
+    age: 25,
+    address: '123 Main Street',
+  };
+
   return (
-    <div className="App">
-      <h1>Hello</h1>
-      <Greet />
-      <Welcome />
+    <div>
+      <h1>My React App</h1>
+      <ChildComponent person={person} />
     </div>
   );
 }
 
-export default App;
+// export default App;
+export default App
+
---
+++ src/Components/Props.js
@@ -0,0 +1,37 @@
+// import React from 'react'
+// function ChildComponent(props) {
+//     return (
+//         <div>
+//             {/* Simple argument passed */}
+//             <p>Name = {props.name}</p>
+//             <p>Age = {props.age}</p>
+
+//             {/* object properties accessed */}
+//             <p>Surname={props.person.surname}</p>
+//             <p>Age={props.age}</p>
+
+//         </div>
+//     );
+// }
+
+// export default ChildComponent;
+// ChildComponent.js
+
+
+
+import React from 'react';
+
+function ChildComponent(props) {
+    // destructuring assignment 
+    const { person } = props;
+
+    return (
+        <div>
+            <p>Name: {person.name}</p>
+            <p>Age: {person.age}</p>
+            <p>Address: {person.address}</p>
+        </div>
+    );
+}
+
+export default ChildComponent;
