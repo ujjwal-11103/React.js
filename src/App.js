bfe7988573d0550ebd7e6c8fdf8e86470d75b72f
Ujjwal Upadhyay <ujjwalupadhyay2003@gmail.com>
2023-12-14 13:08:45 +0530

    Understanding state with functionl component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,10 @@
 import React from 'react';
+import StateFunctionalComp from './Components/StateFunctionalComp';
+function App() {
 
-function App(){
-
-  let data = 'Ujjwal'
-
-  function apple(){
-
-    data='prajwal'
-    console.log(data);
-    alert("Function called")
-    
-  }
-
-  return(
-<div>
-    <h1>Understanding why to use props and state</h1>
-    
-    <h2>Hello {data}</h2>
-
-{/* If we have used state method then it would get upfated */}
-    <h2>Hello again {data}</h2>                
-
-    {/* This is incorrect */}
-    {/* <button onClick={apple()}>Click</button>    */}
-
-    {/* Correct syntax */}
-    <button onClick={()=>apple()}>Click</button>   
-     
+  return (
+    <div>
+      <StateFunctionalComp />
     </div>
   )
 }
---
+++ src/Components/StateFunctionalComp.js
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+import React from 'react'
+
+export default function StateFunctionalComp() {
+
+    const [data, updateData] = useState(0)
+
+    function setData() {
+        updateData(data + 1);
+    }
+
+    console.warn("re-rendered")
+
+    return (
+        <div>
+            <h1> {data} </h1>
+            <button onClick={setData}>Click to Update</button>
+
+        </div>
+    )
+}
