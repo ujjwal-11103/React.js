f2de2cc3e6254db20ff8ac340cf6675556ec5da2
Ujjwal Upadhyay <ujjwalupadhyay2003@gmail.com>
2023-12-17 17:03:53 +0530

    Dealing with bootstrap

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from 'react'
-// external css
-import './Components/style.css'
+import { Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-// external custom css
-import style from './Components/style.module.css'
+
 
 export default function App() {
   return (
     <div>
-      <h1 className='primary'>This is blue in color and is called external css </h1>
-      <h1 style={{ color: 'red' }}>This is in red color and is called inline css</h1>
-      <h1 className={style.module}>This is green in color and is known as custom css</h1>
+      <h1>Hello</h1>
+      <Button>Click</Button> <br /><br />
+      <Button variant="primary">Primary</Button>{' '} <br />
+      <Button variant="secondary">Secondary</Button>{' '}
+      <Button variant="success">Success</Button>{' '}
+      <Button variant="warning">Warning</Button>{' '}
+      <Button variant="danger">Danger</Button>{' '}
+      <Button variant="info">Info</Button>{' '}
+      <Button variant="light">Light</Button>{' '}
+      <Button variant="dark">Dark</Button>
+      <Button variant="link">Link</Button>
     </div>
   )
 }
