be5a0db85c1bbb69dcad014870200435d43cc703
Ujjwal Upadhyay <ujjwalupadhyay2003@gmail.com>
2023-12-19 11:40:40 +0530

    Resusing Components in a loop (map)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react'
-import NestedList from './Components/NestedList'
+import Reuse_components from './Components/Reuse_components'
 export default function App() {
+  const object = [
+    {
+      name: 'Ujjwal', email: 'ujjwal@gmail.com'
+    },
+    {
+      name: 'Prajwal', email: 'prajwal@gmail.com'
+    },
+    {
+      name: 'Raj', email: 'raj@gmail.com'
+    }
+  ]
   return (
     <div>
-      <NestedList />
-    </div>
+      <h1>Reusing components in loop</h1>
+      {
+        object.map((item, i) =>
+          <Reuse_components data={item} />
+        )}
+    </div >
   )
 }
---
+++ src/Components/Reuse_components.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Reuse_components(props) {
+
+
+    return (
+        <div>
+            <h1>User = {props.data.name}</h1>
+
+        </div>
+    )
+}
