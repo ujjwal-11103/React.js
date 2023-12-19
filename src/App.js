29cc03e4d2cc12ec2e7af6291d6b0152a6c40fd1
Ujjwal Upadhyay <ujjwalupadhyay2003@gmail.com>
2023-12-19 12:33:24 +0530

    Fragment (<>) for div purpose

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react'
-import Reuse_components from './Components/Reuse_components'
+import React, { Fragment } from 'react'
+import Fragments from './Components/Fragments'
+// fragmens are use to wrap multiple elements or components 
 export default function App() {
-  const object = [
-    {
-      name: 'Ujjwal', email: 'ujjwal@gmail.com'
-    },
-    {
-      name: 'Prajwal', email: 'prajwal@gmail.com'
-    },
-    {
-      name: 'Raj', email: 'raj@gmail.com'
-    }
-  ]
+
   return (
-    <div>
-      <h1>Reusing components in loop</h1>
-      {
-        object.map((item, i) =>
-          <Reuse_components data={item} />
-        )}
-    </div >
+    // <React.Fragment>
+    // for below method it require import of {fragment}
+    <Fragment>
+      {/* <> */}
+      <h1>hello </h1>
+      <Fragments />
+      {/* </> */}
+    </Fragment>
+    // </React.Fragment>
   )
+
 }
---
+++ src/Components/Fragments.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Fragments() {
+  return (
+    <>
+      <h1>Hello</h1>
+      <h1>Hello2</h1>
+      <h1>Hello3</h1>
+      <h1>Hello4</h1>
+    </> 
+  )
+}
