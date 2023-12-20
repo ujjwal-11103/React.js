7ddcd9dd566669524f2e1d358a3ec750d9f032d4
Ujjwal Upadhyay <ujjwalupadhyay2003@gmail.com>
2023-12-20 13:56:56 +0530

    Pure Cmponent concept using state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import React from 'react'
-import SendingDataChildToPraent from './Components/SendingDataChildToPraent'
-export default function App() {
-
-  function Testing(items) {
-    console.log(items);
+import React, { PureComponent } from 'react'
+// import PureComponentsProps from './Components/PureComponentsProps';
+export default class App extends PureComponent {
+  constructor() {
+    super(); {
+      this.state = {
+        count: 1
+      }
+    }
   }
+  render() {
+    console.log("Re-renderd");
+    return (
+      <div>
+        <h1>Count = {this.state.count}</h1>
+        <button onClick={() => this.setState({ count: 1 })}>Click</button>
 
-  return (
-    <div>
-      <SendingDataChildToPraent data={Testing} />
-    </div>
-  )
+        {/* <PureComponentsProps data={this.state.count} /> */}
+
+
+        {/* Since count here it is one (1) only so since the pagee is not updated  then also it will re render the page*/}
+        {/* therefore to prevent this we use pure component it only updates the page when there is any change in the document(page) */}
+
+      </div>
+    )
+  }
 }
