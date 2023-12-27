d05c3f3ae6d10b938588fdfb3851cdae8dc33529
Ujjwal Upadhyay <ujjwalupadhyay2003@gmail.com>
2023-12-27 23:25:26 +0530

    Local storage commit

--- src/Components/Router/Protected.js
+++ src/Components/Router/Protected.js
@@ -31,3 +31,12 @@ export default function ProtectedComponent(props) {
         </div>
     )
 }
+
+
+// 1. React.js mein local storage ka istemal karke aap client-side par data store aur retrieve kar sakte hain.
+
+// 2.Local storage ek saral key-value store pradan karta hai jo is bat ka dhyan rakhta hai ki jab browser band hota hai aur phir se khulta hai, tab bhi data bana rahta hai.
+
+// 3.Aap local storage ka istemal karke chhote matra mein data, jaise ki upayogakarta ke pasandeeda ya prasanskriti sthiti, ko store karne ke liye kar sakte hain.
+
+
