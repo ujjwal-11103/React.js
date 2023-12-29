7cd32f50ad96ed6905e3da0aa9682cf16a611b3e
Ujjwal Upadhyay <ujjwalupadhyay2003@gmail.com>
2023-12-29 12:29:16 +0530

    Get Method | Fetch AP | optional chaining ( ?. )

--- src/App.js
+++ src/App.js
@@ -1,22 +1,58 @@
-import React from 'react'
-import { Routes, Route, Link } from 'react-router-dom'
-import Links from './Components/Routing/Links'
-import About from './Components/Routing/About'
-import Home from './Components/Routing/Home'
+import React, { useEffect, useState } from 'react';
+
 export default function App() {
+
+  const [weatherData, setWeatherData] = useState([])
+
+  async function checkweather(city) {
+
+    const api_key = "c7848b1913cb882c176585e7bfc7f4c1";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
+
+    const data = await fetch(`${url}`)
+      .then(response => response.json());
+
+    setWeatherData(data);
+    console.log(data);
+
+  }
+
+  useEffect(() => {
+    let city = prompt("Enter city Name = ")
+    checkweather(city);
+  }, []);
+
   return (
-    <div style={{ textAlign: 'center' }}>
+    <div>
+      <h1>Hello</h1>
 
-      <Links />
-      <Routes>
-        <Route path='/Home' element={<Home />} />
-        <Route path='/About' element={<About />} />
-      </Routes>
+      <table style={{ borderCollapse: 'collapse', width: '100%' }}>
+        <tbody>
+          <tr>
+            <td style={cellStyle}>Name</td>
+            <td style={cellStyle}>Temp (℉)</td>
+            <td style={cellStyle}>Pressure (pa)</td>
+            <td style={cellStyle}>Humidity  (g/m3)</td>
+          </tr>
 
+          <tr>
+            <td style={cellStyle}>{weatherData.name}</td>
+            <td style={cellStyle}>{weatherData.main?.temp}</td>
+            <td style={cellStyle}>{weatherData.main?.pressure}</td>
+            <td style={cellStyle}>{weatherData.main?.humidity}</td>
 
+            {/* Optional chaining (?.) ka use isliye kiya gaya hai taki jab aap weatherData.main.temp ya kisi aur nested property ko access karte hain, aur agar koi intermediate property (jaise ki main) undefined hai toh bhi aapko error nahi milega, aur aapka code sahi tarah se chalega.*/}
 
+          </tr>
+
+        </tbody>
+      </table>
     </div>
-  )
+  );
 }
 
-
+const cellStyle = {
+  border: '1px solid black',
+  padding: '8px',
+  textAlign: 'center',
+};
