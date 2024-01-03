6825d66c3f522312530bd5322d256a7d73e7f533
Ujjwal Upadhyay <ujjwalupadhyay2003@gmail.com>
2024-01-03 18:35:09 +0530

    Container - mapDispatchToProps - mapStateToProps

--- src/Containers/CartContainer.js
+++ src/Containers/CartContainer.js
@@ -1,12 +1,33 @@
 import React from 'react'
-
-
+import { connect } from 'react-redux'
+import { addToCart } from '../service/Actions/Action'
 import Cart from '../Components/Cart'
 
-export default function CartContainer() {
-    return (
-        <div>
-            <Cart />
-        </div>
-    )
-}
+
+    const mapStateToProps = state => ({
+
+    })
+
+    const mapDispatchToProps = dispatch => ({
+        addToCartHandler: data => dispatch(addToCart(data))
+    })
+    
+    
+    export default connect(mapDispatchToProps, mapStateToProps)(Cart)
+
+
+// function CartContainer() {
+
+//     const mapStateToProps = state => ({
+
+//     })
+
+//     const mapDispatchToProps = dispatch => ({
+//         addToCartHandler: data => dispatch(addToCart(data))
+//     })
+//     return (
+//         <div>
+//             <Cart />
+//         </div>
+//     )
+// }
